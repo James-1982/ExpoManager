2de939f434f2aa7ddc1be77043c1b14ea714d9de
James <152623678+James-1982@users.noreply.github.com>
2026-03-27 12:56:55 +0100

    Update ApiConstants.cs

--- Expo.API/Utils/ApiConstants.cs
+++ Expo.API/Utils/ApiConstants.cs
@@ -8,7 +8,7 @@ public static class ApiConstants
     /// <summary>
     /// Name of API project
     /// </summary>
-    public const string APINAME = "Hidonix API";
+    public const string APINAME = "Expo API";
     /// <summary>
     /// Version V1
     /// </summary>
